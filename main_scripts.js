292114a83f93bbae5e9289a95282aa525867a16e
carlosvchr <carlosvchr@gmail.com>
2018-04-23 00:51:13 +0200

    Completed footer information. Added terms of use. Project has been separated in several files, one for html, another for style and the other one for scripts. Added terms and conditions html file.

---
+++ main_scripts.js
@@ -0,0 +1,114 @@
+var sidebarWidth = "240px";
+var mobilew = 601;
+function w3_toggleOpen(){
+	if(document.getElementById("sideMenu").style.display == "none"){
+		w3_open();
+	}else{
+		w3_close();
+	}
+}
+
+function w3_open() {
+	if(window.innerWidth < mobilew){
+		document.getElementById("sideMenu").style.width = "100%";
+		document.getElementById("closeSidebar").style.width = "100%";
+	}else{
+		document.getElementById("sideMenu").style.width = sidebarWidth;
+		document.getElementById("closeSidebar").style.width = sidebarWidth;
+		document.getElementById("main").style.marginLeft = sidebarWidth;
+		document.getElementById("footer").style.marginLeft = sidebarWidth;
+	}
+	document.getElementById("sideMenu").style.display = "block";
+}
+
+function w3_close() {
+	document.getElementById("sideMenu").style.display = "none";
+	document.getElementById("main").style.marginLeft = "0";
+	document.getElementById("footer").style.marginLeft = "0";
+}
+
+function loadFunc(){
+	// TABLE::td
+	var tds = document.getElementsByTagName("td");
+	for(var i = 0, j = tds.length; i < j; ++i)
+		tds[i].style.verticalAlign = "middle";
+
+	// SIDEBAR
+	if(window.innerWidth < mobilew){
+		w3_close();
+	}else{
+		w3_open();
+	}
+	
+	// HEADER
+	if(window.innerWidth < mobilew){
+		document.getElementById("openNav").style.top = "60px";
+		document.getElementById("openNav").style.left = "10px";
+		document.getElementById("headerTitle").style.top = "18px";
+		document.getElementById("headerTitle").style.left = "18px";
+		document.getElementById("searchbox").style.top = " 68px";
+		document.getElementById("searchbox").style.right = "18px";
+		document.getElementById("searchbox").style.width = "200px";
+		document.getElementById("headerin").style.height = "112px";
+		document.getElementById("headerSeparator").style.height = "112px";
+	}else{
+		document.getElementById("openNav").style.top = "10px";
+		document.getElementById("openNav").style.left = "10px";
+		document.getElementById("headerTitle").style.top = "18px";
+		document.getElementById("headerTitle").style.left = "92px";
+		document.getElementById("searchbox").style.top = " 18px";
+		document.getElementById("searchbox").style.right = "18px";
+		document.getElementById("searchbox").style.width = "340px";
+		document.getElementById("headerin").style.height = "72px";
+		document.getElementById("headerSeparator").style.height = "72px";
+	}
+	
+}
+
+function gotoRef(ref){
+	window.location.href = ref;
+	if(window.innerWidth < mobilew){
+		window.scrollBy(0, -112);
+	}else{
+		window.scrollBy(0, -72);
+	}
+}
+
+function setSearchboxItems(){
+	if(event.key === 'Enter') {
+		gotoRef("#"+document.getElementById("searchbox").value.replace('-', '')+"ref");
+	}
+	
+	if(document.getElementById("searchbox").value.length>1){
+		if(document.getElementById("searchlist").innerHTML.length==0){
+			var itsearch = ["accordion", "dropdown", "hbox", "modal", "radiogroup", "sidebar", "tabbedbox", "table", "vbox",
+			"audio", "button", "checkbox", "image", "label", "radiobutton", "separator", "textfield", "video",
+			"align", "alt", "animation", "autoplay", "bgcolor", "border", "border-color", "border-radius", "class",
+			"controls", "delay", "effect", "elevation", "font-family", "font-size", "height", "id", "link", "loop",
+			"margin", "muted", "onchange", "onclick", "padding", "placeholder", "poster", "preload", "selected",
+			"src", "text", "text-align", "text-color", "text-decoration", "tooltip", "width"];
+			var toappend = "";
+			for(var i=0; i<itsearch.length; i++){
+				toappend += "<option value="+itsearch[i]+">"
+			}
+			document.getElementById("searchlist").innerHTML = toappend;
+		}
+	}else{
+		document.getElementById("searchlist").innerHTML = "";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("goUpBtn").style.display = "block";
+	} else {
+		document.getElementById("goUpBtn").style.display = "none";
+	}
+}
